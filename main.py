f35d90bdb9ce464376c033e45bb48c39a7962865
Felipe Santos <tfelipesantos99s@gmail.com>
2025-06-27 22:28:03 -0300

    Add validations to register view

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect
 import os
 
+from dotenv import load_dotenv
+from flask import Flask, flash, render_template, request, redirect, url_for
+
 app = Flask(__name__)
+app.secret_key = os.environ["SECRET_KEY"]
 
 @app.route("/")
 @app.route("/index")
@@ -13,8 +15,27 @@ def home():
 def login():
     return render_template("login.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET","POST"])
 def register():
+    if request.method == "POST":
+        full_name = request.form['full_name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        error=False
+
+        if not (full_name or email or password or confirm_password):
+            flash("Por favor preencha todos os campos","error")
+            error=True      
+
+        if password != confirm_password:
+            flash("As senhas não correspondem","error")
+            error=True      
+
+        if not error:
+            flash('Cadastro realizado com sucesso',"success")
+            return redirect(url_for('login'))
+    
     return render_template("register.html")
 
 if __name__ == "__main__":
